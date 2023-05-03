0c8c32ba60a280405b8fc16de3114cfcdaaca8de
Rohan Deo <rohandeo307@gmail.com>
2023-05-03 01:04:54 -0700

    Completed create note function which allows users to create a new note when a date is clicked. After this the date will be stored to the db

--- studybuddy.py
+++ studybuddy.py
@@ -18,6 +18,58 @@ Event Properties
 
 """""
 
+class Database_Controller():
+    def __init__(self):
+        self.path = "./database.db"
+        
+        self.conn = self.create_connection()
+        self.create_note_table()
+        
+    
+    def create_connection(self):
+        conn = None
+        try:
+            conn = sqlite3.connect("./sqliteDB/database.db")
+            return conn
+        except Error as e:
+            print(e)
+
+        return conn
+        
+    def create_note_table(self):
+        sql_create_note_table = """ CREATE TABLE IF NOT EXISTS note(
+                                        id integer PRIMARY KEY,
+                                        description text,
+                                        begin_date text,
+                                        end_date text,
+                                        completion_status integer,
+                                        title text
+                                    ); """
+        
+        
+        if self.conn is not None:
+            c = self.conn.cursor()
+            c.execute(sql_create_note_table)
+    
+    def dataInsert(self, data):
+        if data is None:
+            return
+        else:
+            self.data = data
+        print(self.data)
+        title = self.data['title']
+        tags = self.data['tags']
+        description = self.data['description']
+        start_date = self.data['start_date']
+        end_date = self.data['end_date']
+        status = self.data['status']
+        print(title,tags,description,start_date,end_date,status)
+        sql = """INSERT INTO note(description,begin_date,end_date,completion_status,title) VALUES(?,?,?,?,?);""" 
+        params = (description,start_date,end_date,status,title)
+        c = self.conn.cursor()
+        c.execute(sql,params)
+        self.conn.commit()
+
 
 class Main(QMainWindow):
     def __init__(self):
@@ -25,8 +77,45 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         # Load UI file created in Qt Designer
         loadUi("untitled.ui", self)
+        self.connectDB = Database_Controller()
+
+        self.calendarWidget.clicked.connect(self.create_note)
+        self.buttonCancel.clicked.connect(self.set_default)
+        self.buttonSubmit.clicked.connect(self.submit_button)
 
     # Application Functions
+
+    def submit_button(self):
+        while True:
+            self.stackedWidget.setCurrentIndex(0)
+            tags = self.lineEditTags.text()
+            title = self.lineEditTitle.text()
+            description = self.lineEditDescription.text()
+            start_date = self.dateEdit.text()
+            end_date = self.dateEdit_2.text()
+            status = self.horizontalSlider.value()
+            self.lineEditTags.clear()
+            self.lineEditTitle.clear()
+            self.lineEditDescription.clear()
+            self.dateEdit.clear()
+            self.dateEdit_2.clear()
+            self.horizontalSlider.setValue(0)
+            data = {
+                "title": title,
+                "tags" : tags,
+                "description": description,
+                "start_date" : start_date,
+                "end_date" : end_date,
+                "status" : status
+            }
+            self.connectDB.dataInsert(data)
+            # print(f'{tags}, {title}, {description}, {start_date}, {end_date}, {status}')
+            break
+
+    
+
+    def set_default(self):
+        self.stackedWidget.setCurrentIndex(0)
 
     def calendar_edit(self):
         pass
@@ -34,18 +123,21 @@ class Main(QMainWindow):
     def edit_note(self):
         pass
 
+    def create_note(self):
+        self.stackedWidget.setCurrentIndex(1)
+    
     # Database Functions
 
-    def create_connection(self):
-        conn = None
-        try:
-            conn = sqlite3.connect("./database.db")
-            print(sqlite3.version)
-        except Error as e:
-            print(e)
-        finally:
-            if conn:
-                conn.close()
+    # def create_connection(self):
+    #     conn = None
+    #     try:
+    #         conn = sqlite3.connect("./database.db")
+    #         print(sqlite3.version)
+    #     except Error as e:
+    #         print(e)
+    #     finally:
+    #         if conn:
+    #             conn.close()
             
     def create_event(self):
         """
@@ -85,3 +177,4 @@ if __name__ == "__main__":
     app.exec_()
 
     
+sql = """insert into note(description,begin_date,end_date,completion_status,title) values(?,?,?)""", (description,title,start_date,end_date,status)
